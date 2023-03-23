b8b84fcf45029a4d18ca39b5ecae33a4b06f8eb2
이승희 <87055720+seung127@users.noreply.github.com>
2023-03-23 19:59:43 +0900

    Docs: 2주차 과제

---
+++ ex02/Calculator.js
@@ -0,0 +1,39 @@
+let re=0;
+let operation="+";
+let first=0;
+let second=0;
+
+function handleOnChange(e) {
+    var sign  = document.getElementById("sign");
+	var op = (sign.options[sign.selectedIndex].value);
+
+    if (op=="+") operation="+";
+    else if (op=="-") operation="-";
+    else if (op=="*") operation="*";
+    else if (op=="/") operation="/";
+	
+	
+  }
+
+  function setValue() {
+
+	first = parseInt(document.getElementById('first').value); 
+	second = parseInt(document.getElementById('second').value);
+
+    if(operation=="-"){
+	re = first - second;
+    document.getElementById("result").innerText=re;}
+
+    else if(operation=="*"){
+	re = first * second;
+    document.getElementById("result").innerText=re;}
+
+    else if(operation=="/"){
+	re = first / second;
+    document.getElementById("result").innerText=re;}
+
+    else if(operation=="+"){
+        re = first + second;
+        document.getElementById("result").innerText=re;}
+    
+}
---
+++ ex03/Alarm.js
@@ -0,0 +1,27 @@
+let alarmTime;
+let isAlarmSet=false;
+let currenttime;
+let currentTimeInterval;
+let hour;
+let minute;
+let timeString;
+
+function setAlarm(){
+    alarmTime=document.getElementById("time").value;
+    isAlarmSet= true;
+     if(isAlarmSet){
+        currentTimeInterval= setInterval(()=>{
+            currenttime=new Date();
+            hour=('0'+currenttime.getHours()).slice(-2);
+            minute=('0'+currenttime.getMinutes()).slice(-2);
+            timeString=hour + ':' + minute;
+    
+        if(alarmTime==timeString){
+                alert("알람");
+                setTimeout(() => {
+                    clearInterval(currentTimeInterval);
+                }, 60000);
+            }
+        },1000);
+    }
+}
